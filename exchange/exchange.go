9dc01a4888e483e768ad2c6be034c5c393d9ac14
hom <hcclinux@gmail.com>
2021-01-22 21:04:09 +0800

    fix exchange

--- exchange/exchange.go
+++ exchange/exchange.go
@@ -1,43 +1,10 @@
 package exchange
 
 import (
-	"time"
-
-	"github.com/nntaoli-project/goex"
-	"github.com/nntaoli-project/goex/builder"
 )
 
-// Exchange .
-type Exchange struct {
-	api 		goex.API
-	opts 		Options
+// Exchange is a exchange interface
+type Exchange interface {
+
 }
 
-// NewExchange .
-func NewExchange(options ...Option) (ex *Exchange) {
-	ex = &Exchange{
-		opts: Options{},
-	}
-
-	for _, o := range options {
-		o(&ex.opts)
-	}
-	return
-}
-
-// Init .
-func (ex *Exchange) Init(opts ...Option) error {
-	for _, o := range opts {
-		o(&ex.opts)
-	}
-
-	build := builder.NewAPIBuilder().HttpTimeout(10 * time.Second).HttpProxy(ex.opts.Proxy)
-	ex.api = build.APIKey(ex.opts.APIKey).APISecretkey(ex.opts.SecretKey).Build(ex.opts.Name)
-
-	return nil
-}
-
-// GetBalance .
-func (ex *Exchange) GetBalance() (*goex.Account, error) {
-	return ex.api.GetAccount()
-}
--- exchange/exchange_test.go
+++ exchange/exchange_test.go
@@ -2,20 +2,8 @@ package exchange
 
 import (
 	"testing"
-
-	"github.com/nntaoli-project/goex"
 )
 
 func TestNewExchange(t *testing.T) {
-	ex := NewExchange(
-		WithExchangeName(goex.BINANCE),
-		WithProxy("socks5://127.0.0.1:7890"),
-		APIKey(""),
-		SecretKey(""),
-	)
-	if err := ex.Init(); err != nil {
-		t.Error(err)
-		return
-	}
-	t.Log(ex.GetBalance())
+
 }
